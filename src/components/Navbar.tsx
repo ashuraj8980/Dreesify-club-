60e8ef0176c9c8a41e63c3a717a3c65e939c88f8
ashukumar8076801908 <ashukumar8076801908@gmail.com>
2026-05-17 11:13:09 +0000

    Revert home page and change login button color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Search, Heart, ShoppingBag, User, Menu, X, ChevronRight, LogOut } from 'lucide-react';
@@ -96,7 +97,7 @@ export default function Navbar() {
               ) : (
                 <button 
                   onClick={login} 
-                  className="flex items-center space-x-2 md:space-x-3 group bg-brand-black text-white px-3 md:px-4 py-2 hover:bg-brand-gold hover:text-brand-black transition-all duration-500 rounded-sm"
+                  className="flex items-center space-x-2 md:space-x-3 group bg-red-500 text-white px-3 md:px-4 py-2 hover:bg-red-700 hover:text-white transition-all duration-500 rounded-sm"
                 >
                   <User className="w-[16px] h-[16px] group-hover:scale-110 transition-transform" />
                   <span className="text-[9px] font-black uppercase tracking-[0.2em]">Login</span>
